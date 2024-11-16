f243b9ad801f7db8a3aaa89432261c8ed3bce1dc
designermanjeets@gmail.com <designermanjeets@gmail.com>
2024-11-16 11:24:33 +0530

    Stripe Payment History

--- src/app/pages/mockinto-history/mockinto-history.component.ts
+++ src/app/pages/mockinto-history/mockinto-history.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { StripeMockintoService } from '../services/stripe.service';
 
 @Component({
   selector: 'app-mockinto-history',
   templateUrl: './mockinto-history.component.html',
   styleUrls: ['./mockinto-history.component.scss']
 })
-export class MockintoHistoryComponent {
+export class MockintoHistoryComponent implements OnInit {
+
+  allPayments: any = [];
+
+  constructor(
+    private cdRef: ChangeDetectorRef,
+    private plutoService: StripeMockintoService
+  ) { }
+
+  ngOnInit(): void {
+    this.fetchAllPayments();
+  }
+
+  fetchAllPayments() {
+    this.plutoService.getPaymentHistory().subscribe((res) => {
+      if(res) {
+        this.allPayments = res.data;
+        this.cdRef.detectChanges();
+      }
+    });
+  }
 
 }
